46004da67511492ff0f43b605a1190d5ba005084
Bastien <45488913+BastienBiblocque@users.noreply.github.com>
2023-09-21 21:53:13 +0200

    Init spring project

---
+++ src/main/java/com/example/demo/MnsSpringCollocationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnsSpringCollocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnsSpringCollocationApplication.class, args);
+	}
+
+}
