6150505c8b48e65339b7f2aa82be9d60059633d9
Bastien <45488913+BastienBiblocque@users.noreply.github.com>
2023-09-26 09:38:17 +0200

    Passage en service

--- src/main/java/com/example/demo/controller/HouseController.java
+++ src/main/java/com/example/demo/controller/HouseController.java
@@ -4,7 +4,12 @@ import com.example.demo.model.Bed;
 import com.example.demo.model.House;
 import com.example.demo.repository.BedRepository;
 import com.example.demo.repository.HouseRepository;
+import com.example.demo.service.BedService;
+import com.example.demo.service.HouseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,55 +20,58 @@ import java.util.Set;
 @CrossOrigin(origins="*")
 public class HouseController {
 
-    private final HouseRepository repository;
-    private final BedRepository bedRepository;
-    public HouseController(HouseRepository repository, BedRepository bedRepository) {
-        this.repository = repository;
-        this.bedRepository = bedRepository;
+    private final HouseService houseService;
+    private final BedService bedService;
+    public HouseController(HouseService houseService, BedService bedService) {
+        this.houseService = houseService;
+        this.bedService = bedService;
+
     }
 
-    @GetMapping("")
-    public Object get() {
-        return repository.findAll();
+    @GetMapping({"", "/"})
+    public ResponseEntity<List<House>> getAll(){
+        return ResponseEntity.ok(houseService.getAll());
     }
 
     @GetMapping("/{id}")
-    public Object getOne(@PathVariable(value="id") String id) {
-        return repository.findById(Long.valueOf(id));
+    public ResponseEntity<House> getById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(houseService.getById(id));
     }
 
     @PostMapping("/add")
-    public void post(@RequestParam("name") String name, @RequestParam("bed") Integer bed) {
+    public House post(@RequestParam("name") String name, @RequestParam("bed") Integer bed) {
         House house = new House(name, bed);
-        repository.save(house);
+        houseService.save(house);
 
         for (int i = 0; i < bed; i++) {
             Bed newBed = new Bed(house);
-            bedRepository.save(newBed);
+            bedService.save(newBed);
         }
+        return house;
     }
 
     @PostMapping("/{id}/book")
-    public void book(@PathVariable(value="id") String id, @RequestParam("quantity") Integer quantity, @RequestParam("userId") String userId) {
+    public void book(@PathVariable(value="id") Long id, @RequestParam("quantity") Integer quantity, @RequestParam("userId") String userId) {
         //check availability
-        House house = repository.getById(Long.valueOf(id));
+        House house = houseService.getById(id);
         Set<Bed> beds = house.getBeds();
 
         List<Bed> bedsAvailable = new ArrayList();
         for (Bed bed : beds) {
-            System.out.println(bed);
             if (bed.getUserId() == null) {
                 bedsAvailable.add(bed);
             }
         }
 
         if (bedsAvailable.size() < quantity) {
-            // blabla error
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Not enough bed available"
+            );
         }
 
         for (int i = 0; i < quantity; i++) {
             bedsAvailable.get(i).setUserId(userId);
-            bedRepository.save(bedsAvailable.get(i));
+            bedService.save(bedsAvailable.get(i));
         }
     }
 }
